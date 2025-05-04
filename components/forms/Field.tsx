55ed365db4a868b41ec6fe5f8310aa16ed8599d1
Zakich95 <chergui.zakaria.1995@gmail.com>
2025-05-04 13:24:54 +0100

    Delete components directory

--- components/forms/Field.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Field = ({ children }: { children: React.ReactNode }) => (
-  <div className='my-2 flex flex-col'>
-    {children}
-  </div>
-)
-
-export default Field;
